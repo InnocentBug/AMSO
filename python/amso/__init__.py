c36f1d37be0c94275c1a91305dde08899810e705
Ludwig Schneider <ludwigschneider@uchicago.edu>
2025-02-05 16:49:09 -0600

    Introducing a MemArray class that handles GPU memory, that can be read from python.

--- python/amso/__init__.py
+++ python/amso/__init__.py
@@ -1,3 +1,7 @@
 from ._amso import print_platform_info
+from .memarray import MemArray
 
-__all__ = ["print_platform_info"]
+__all__ = [
+    "print_platform_info",
+    "MemArray",
+]
--- python/module_amso.cpp
+++ python/module_amso.cpp
@@ -1,9 +1,13 @@
-#include "platform_status.h"
 #include <pybind11/pybind11.h>
+
+#include "memarray.h"
+#include "platform_status.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(_amso, m) {
   m.def("print_platform_info", &printPlatformInfo,
         "Log information about the current system setup.");
+
+#include "memarray_pybind.incl"
 }
